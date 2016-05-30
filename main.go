44e348a92ab6a1be68ecc0d4dc9d3ed998a8a6c9
John Gosset <john.gosset@gmail.com>
2016-05-30 16:22:55 -0400

    Add init functionality

--- main.go
+++ main.go
@@ -16,12 +16,32 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+type Layout struct {
+	Dirs  []string
+	Files []string
+}
+
+const (
+	usage = `elx: A simple static site generator.
+John Gosset 2016 (MIT License)
+
+elx init [<DIR>]
+elx build [<DIR>]
+`
+)
+
 var (
 	// directory layout to be generated via "init" subcommand
-	DirLayout = []string{
-		"aaa",
-		"bbb",
-		"ccc",
+	DefLayout = &Layout{
+		Dirs: []string{
+			"_site",
+			"_includes",
+			"_layouts",
+			"_posts",
+		},
+		Files: []string{
+			"config.toml",
+		},
 	}
 )
 
@@ -32,8 +52,30 @@ type Post struct {
 }
 
 func main() {
+	flag.Usage = func() {
+		fmt.Println(usage)
+	}
+	basePath := flag.String("b", ".", "base path")
 	src := flag.String("d", "src", "source directory")
 	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	switch args[0] {
+	case "init":
+		if len(args) == 2 {
+			basePath = &args[1]
+		}
+		err := initDirLayout(*basePath, DefLayout)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 
 	// find markdown files
 	matches, err := filepath.Glob(*src + "/*.md")
@@ -49,6 +91,23 @@ func main() {
 		}
 		fmt.Fprintf(os.Stdout, "%s\n", toHTML(dat))
 	}
+}
+
+func initDirLayout(basepath string, layout *Layout) error {
+	defer fmt.Println("Initialized elx directory:", basepath)
+	for _, d := range layout.Dirs {
+		path := filepath.Join(basepath, d)
+		os.MkdirAll(path, 0775)
+	}
+
+	for _, f := range layout.Files {
+		path := filepath.Join(basepath, f)
+		err := ioutil.WriteFile(path, []byte{}, 0644)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	return nil
 }
 
 func toHTML(input []byte) []byte {
